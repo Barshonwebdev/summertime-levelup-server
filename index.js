7c70aacefa9ffdc4a2a8bb2062404dfd8a0b3ad0
Barshon <barshonweb@gmail.com>
2023-12-23 11:09:28 +0600

    user role update and user delete api

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 // mongodb database connect 
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri =
   `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7di2jdk.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -47,6 +47,40 @@ async function run() {
 
     app.get('/users',async(req,res)=>{
       const result=await userCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.delete('/users/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query={_id:new ObjectId(id)};
+      const result=await userCollection.deleteOne(query);
+      res.send(result);
+    })
+    //make admin api
+    app.patch('/users/admin/:id',async(req,res)=>{
+      const id=req.params.id;
+      const filter={_id:new ObjectId(id)};
+      const updateDoc={
+        $set:{
+          role:'admin'
+        },
+      }
+
+      const result= await userCollection.updateOne(filter,updateDoc);
+      res.send(result);
+    })
+
+    //make instructor api
+    app.patch('/users/instructor/:id',async(req,res)=>{
+      const id=req.params.id;
+      const filter={_id:new ObjectId(id)};
+      const updateDoc={
+        $set:{
+          role:'instructor'
+        },
+      }
+
+      const result= await userCollection.updateOne(filter,updateDoc);
       res.send(result);
     })
     // Send a ping to confirm a successful connection
