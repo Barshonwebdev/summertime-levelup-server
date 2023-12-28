e6325fabc327e3ad06194a68f9a5d0b44e78adb0
Barshon <barshonweb@gmail.com>
2023-12-28 13:37:17 +0600

    dynamic update class data api

--- index.js
+++ index.js
@@ -156,6 +156,14 @@ async function run() {
       const result=await classesCollection.find(query).toArray();
       res.send(result);
     })
+
+    // update each class api 
+    app.get('/updateclass/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query={_id: new ObjectId(id)};
+      const result=await classesCollection.findOne(query);
+      res.send(result);
+    })
     //approve class api
     app.patch('/classses/approved/:id',async(req,res)=>{
       const id=req.params.id;
