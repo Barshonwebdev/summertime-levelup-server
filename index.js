4a995402951a99e1e2465297c66c5c17b7337dd9
Barshon <barshonweb@gmail.com>
2023-12-30 19:58:11 +0600

    classes delete api for admin

--- index.js
+++ index.js
@@ -183,7 +183,13 @@ async function run() {
     });
 
 
-
+    // delete class api 
+     app.delete("/classesquery/:id", async (req, res) => {
+       const id = req.params.id;
+       const query = { _id: new ObjectId(id) };
+       const result = await classesCollection.deleteOne(query);
+       res.send(result);
+     });
 
     // instructor api 
 
